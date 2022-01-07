f41c3e0441bd2c7437590d823eb3dc0f24ed9e17
utkarsh <robbin7838@gmail.com>
2022-01-07 19:49:20 +0530

    lib

---
+++ src/textComp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const TextComp = () => {
+  let text;
+  // useEffect(() => {
+  //   text = 'Hello New World';
+  // }, []);
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h2>{text}</h2>
+    </>
+  );
+};
+
+export { TextComp };
